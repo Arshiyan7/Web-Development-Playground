4bdc0383a4b9db5dc872ef88dd8403eb3e9a4e35
Arshiyan <Arshiyankhan92@gmail.com>
2025-11-13 22:33:14 +0500

    Added HTML and JS for Quiz end page

---
+++ JavaScript/Quiz_app/end.js
@@ -0,0 +1,40 @@
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreBtn');
+const finalScore = document.querySelector('#finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+
+const MAX_HIGH_SCORES = 5;
+
+
+finalScore.innerText = mostRecentScore;
+
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+// Save score function
+saveScoreBtn.addEventListener('click', e => {
+    e.preventDefault(); 
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+
+    
+    highScores.push(score);
+
+    // Sort and keep top 5
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES);
+
+    
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    
+    window.location.assign('highscores.html');
+});
