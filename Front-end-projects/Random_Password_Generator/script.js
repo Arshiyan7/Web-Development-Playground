d87380087c563526f32403d7e16964d65c6a0613
Arshiyan <Arshiyankhan92@gmail.com>
2026-01-14 18:56:13 +0500

    Updated JavaScript of random password generator

--- Front-end-projects/Random_Password_Generator/script.js
+++ Front-end-projects/Random_Password_Generator/script.js
@@ -1,1 +1,35 @@
-// To be Added
+const btn1 = document.querySelector('.btn')
+const input = document.querySelector('.input')
+const copy = document.querySelector('.fa-copy')
+const alert_container = document.querySelector('.alert-container')
+
+btn1.addEventListener('click', function () {
+    createPassword()
+})
+copy.addEventListener('click', function () {
+    copyText()
+    if (input.value) {
+        alert_container.classList.remove("active")
+        setTimeout(() => {
+            alert_container.classList.add('active')
+        }, 2000);
+    }
+})
+function createPassword() {
+    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:'.<>?/`~"
+    const password_length = 14
+    let password = ''
+    for (let i = 0; i < password_length; i++) {
+        const randomNum = Math.floor(Math.random() * characters.length)
+        password += characters.substring(randomNum, randomNum + 1)
+        // console.log(randomNum, password) use this to see the random password in the console
+        input.value = password
+        alert_container.classList.add('active')
+    }
+}
+
+function copyText() {
+    input.select()
+    input.setSelectionRange(0, 9999)
+    navigator.clipboard.writeText(input.value)
+}
