6d1a3c57d244eaf5c7afe51d3d0b59124305239a
Arshiyan <Arshiyankhan92@gmail.com>
2026-02-04 15:12:52 +0500

    Updated JS of  Front-end-projects/Expense_Tracker/

--- Front-end-projects/Expense_Tracker/script.js
+++ Front-end-projects/Expense_Tracker/script.js
@@ -1,39 +1,97 @@
-const balanceEl = document.querySelector("#balance")
-const incomeAmountEl = document.querySelector("#income-amount")
-const expenseAmountEl = document.querySelector("#expense-amount")
-const transactionFormEl = document.querySelector("#transaction-form")
-const transactionlistEl = document.querySelector("#transaction-list")
-const descriptionEl = document.querySelector("#description")
-const amountEl = document.querySelector("#amount")
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
 
-// Get what's in local storage if something('transaction') if empty []
-const transactions = JSON.parse(localStorage.getItem('transactions')) || []
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
-transactionFormEl.addEventListener('submit',addTransaction)
+transactionFormEl.addEventListener("submit", addTransaction);
 
-function addTransaction(e){
-    e.preventDefault()
+function addTransaction(e) {
+    e.preventDefault();
 
-    // get values from form
-    const description = descriptionEl.value.trim()
-    const amount = parseFloat(amountEl.value) // here we use parse.float to get values in float form because by default values are in string form
-    //  console.log(typeof amount) //use this in order to check for the type of amount before and after removing parse.float
+    // get form values
+    const description = descriptionEl.value.trim();
+    const amount = parseFloat(amountEl.value);
+
     transactions.push({
-        id:Date.now(),
+        id: Date.now(),
         description,
-        amount
-    })
+        amount,
+    });
 
-    localStorage.setItem('transactions',JSON.stringify(transactions))
+    localStorage.setItem("transactions", JSON.stringify(transactions));
 
-    updateTransactionList()
-    updateSummary()
+    updateTransactionList();
+    updateSummary();
 
-    transactionFormEl.reset()
+    transactionFormEl.reset();
 }
 
-function updateTransactionList(){
-    transactionlistEl.innerHTML = ""
-    
-    const sortedTransactions = [...transactions].reverse()
+function updateTransactionList() {
+    transactionListEl.innerHTML = "";
+
+    const sortedTransactions = [...transactions].reverse();
+
+    sortedTransactions.forEach((transaction) => {
+        const transactionEl = createTransactionElement(transaction);
+        transactionListEl.appendChild(transactionEl);
+    });
 }
+
+function createTransactionElement(transaction) {
+    const li = document.createElement("li");
+    li.classList.add("transaction");
+    li.classList.add(transaction.amount > 0 ? "income" : "expense");
+
+    li.innerHTML = `
+    <span>${transaction.description}</span>
+    <span>
+    ${formatCurrency(transaction.amount)}
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+    </span>`;
+
+    return li;
+}
+
+function updateSummary() {
+    // 100, -50, 200, -200 => 50
+    const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+
+    const income = transactions
+        .filter((transaction) => transaction.amount > 0)
+        .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+    const expenses = transactions
+        .filter((transaction) => transaction.amount < 0)
+        .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+    // update ui => todo: fix the formatting
+    balanceEl.textContent = formatCurrency(balance);
+    incomeAmountEl.textContent = formatCurrency(income);
+    expenseAmountEl.textContent = formatCurrency(expenses);
+}
+
+function formatCurrency(number) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    }).format(number);
+}
+
+function removeTransaction(id) {
+    // filter out the one we wanted to delete
+    transactions = transactions.filter((transaction) => transaction.id !== id);
+
+    localStorage.setItem("transcations", JSON.stringify(transactions));
+
+    updateTransactionList();
+    updateSummary();
+}
+
+// initial render
+updateTransactionList();
+updateSummary();
