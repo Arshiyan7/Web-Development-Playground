55b1b1a000a37da670afa855265dca81c8c79f4c
Arshiyan <Arshiyankhan92@gmail.com>
2026-03-17 16:04:58 +0500

    Updated JS of  Front-end-projects/Country-Explorer/ folder

---
+++ Front-end-projects/Country-Explorer/script.js
@@ -0,0 +1,15 @@
+// TESTING API FETCH
+
+// async function Countries() {
+//     try {
+//         const country_data = await fetch(
+//             "https://restcountries.com/v3.1/name/Pakistan?fields=name,capital,region,subregion,population,currencies,languages,flags"
+//         );
+//         const data = await country_data.json(); 
+//         console.log(data[0]); 
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// Countries();
