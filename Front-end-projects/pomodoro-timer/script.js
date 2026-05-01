c1ead9cfdc4e7f30038a42cbb29ad568f3ed2f9f
Arshiyan <Arshiyankhan92@gmail.com>
2026-05-01 12:43:17 +0500

    added pomodoro project folder with html, css and js files

---
+++ Front-end-projects/pomodoro-timer/script.js
@@ -0,0 +1,6 @@
+const progressArc = document.getElementById('progress-arc');
+const timeDisplay = document.getElementById('time-display');
+const startPauseBtn = document.getElementById('start-pause-btn');
+const modeBtn = document.getElementById('mode-btn');
+const emoji = document.querySelector('.emoji');
+const modeLabel = document.querySelector('.text-display');
