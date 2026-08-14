ca3c416921a16660d8604eb21873936653dd3e3a
Arshiyan Khan <arshiyankhan92@gmail.com>
2026-08-14 22:23:58 +0300

    Updated React/React-concepts/src/main.jsx

--- React/React-concepts/src/main.jsx
+++ React/React-concepts/src/main.jsx
@@ -62,7 +62,7 @@
 // const root = createRoot(document.getElementById("root"))
 // root.render(<MainComponent />)
 
-// -- JS in React 
+// -- JS in React
 // import { createRoot } from "react-dom/client";
 // const root = document.getElementById('root')
 
@@ -71,7 +71,6 @@
 // h1.className = "header"
 
 // root.appendChild(h1)
-
 
 // import { createRoot } from "react-dom/client"
 // import reactLogo from "./assets/react.svg"
@@ -94,3 +93,97 @@
 
 // TILL THIS POINT IT IS NOT EFFECTIVE TO WRITE EVERYTHING INSIDE ROOT.RENDER, WE'LL BE MAKING CUSTOM COMPONENTS NOW!
 // (BASICALLY COMPONENTS FOLDER WHICH WILL HAVE FILES FOR EACH FUNCTION AND WE RENDER THEM HERE THEN)
+// COMPONENTS:
+
+// import { createRoot } from "react-dom/client"
+// import reactLogo from "./assets/react.svg"
+// const root = createRoot(document.getElementById("root"));
+
+// root.render(
+//     <About_react />
+// );
+
+// function About_react() {
+// return (
+//     <>
+//     <header>
+//         <img src={reactLogo} alt="reactLogo" />
+//     </header>
+//     <main>
+//         <h1>Reasons I'm excited to learn React</h1>
+//     <ol>
+//         <li>Build fast — Create modern, interactive websites quickly.</li>
+//         <li>Career opportunities — React is widely used by companies, so it’s a valuable skill.</li>
+//         <li>Fun & flexible — Build cool projects with reusable components and tons of tools.</li>
+//     </ol>
+//     </main>
+//     <footer>
+//         <small>© 2026 Flex developers. All rights reserved</small>
+//     </footer>
+//     </>
+// );
+// }
+
+//Parent/Child component
+// import { createRoot } from "react-dom/client";
+// import reactLogo from "./assets/react.svg"
+// const root = createRoot(document.getElementById("root"));
+
+// root.render(<Page />);
+
+// function Page() {
+//   return (
+//     <>
+//     <Header/>
+//       <main>
+//         <h1>Reasons I'm excited to learn React</h1>
+//         <ol>
+//           <li>Build fast — Create modern, interactive websites quickly.</li>
+//           <li>
+//             Career opportunities — React is widely used by companies, so it’s a
+//             valuable skill.
+//           </li>
+//           <li>
+//             Fun & flexible — Build cool projects with reusable components and
+//             tons of tools.
+//           </li>
+//         </ol>
+//       </main>
+//       <footer>
+//         <small>© 2026 Flex developers. All rights reserved</small>
+//       </footer>
+//     </>
+//   );
+// }
+
+// function Header() {
+//   return (
+//     <header>
+//       <img src={reactLogo} alt="reactLogo" />
+//     </header>
+//   );
+// }
+
+// Using CSS
+import { createRoot } from "react-dom/client";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Header from "./component/header"
+import Main from "./component/MainContent"
+import Footer from "./component/Footer"
+const root = createRoot(document.getElementById("root"));
+
+root.render(<Page />);
+
+
+
+
+function Page(){
+    return(
+        <>
+        <Header/>
+        <Main/>
+        <Footer/>
+        </>
+    )
+}
