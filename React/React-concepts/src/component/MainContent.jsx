abd8950c591e69f862f793b16253475bc2748079
Arshiyan Khan <arshiyankhan92@gmail.com>
2026-08-15 11:51:47 +0300

    Added  React/React-concepts/src/component/ folder to React-concepts

---
+++ React/React-concepts/src/component/MainContent.jsx
@@ -0,0 +1,16 @@
+export default function MainContent() {
+  return (
+    <>
+      <main className="Main">
+        <h1>Fun Facts About React</h1>
+        <ul className="Main-ul">
+          <li>React was created by Facebook in 2013.</li>
+          <li>React components can be reused like LEGO blocks.</li>
+          <li>It uses a Virtual DOM to make UI updates faster.</li>
+          <li>The name “React” refers to how the UI reacts to changes in data</li>
+          <li>React is now maintained by Meta and the open-source community.</li>
+        </ul>
+      </main>
+    </>
+  );
+}
