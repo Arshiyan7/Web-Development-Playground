62d7d4a0dbdbc4b7996681d11b7b2e111bb050a2
Arshiyan Khan <arshiyankhan92@gmail.com>
2026-08-16 21:51:27 +0300

    Added ProjectOne Folder and moved the Header, MainContent and Footer files to it

---
+++ React/React-concepts/src/component/MiniProjectOne/Footer.jsx
@@ -0,0 +1,7 @@
+export default function FooterContent() {
+  return (
+    <footer>
+        <small>© 2026 Flex developers. All rights reserved</small>
+    </footer>
+  );
+}
---
+++ React/React-concepts/src/component/MiniProjectOne/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="Header">
+      <img src="src/assets/react.svg" alt="reactLogo" />
+      <p>ReactFacts</p>
+    </header>
+  );
+}
