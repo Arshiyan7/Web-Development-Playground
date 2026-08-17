b3170c88bc972cd857dfbd892cd87784b3c14331
Arshiyan Khan <arshiyankhan92@gmail.com>
2026-08-17 22:02:31 +0300

    Modified App.css(added Css for props) and App.jsx(added props code)

--- React/React-concepts/src/App.jsx
+++ React/React-concepts/src/App.jsx
@@ -1,11 +1,30 @@
-import Header from './component/MiniProjectTwo/Header'
-import About from './component/MiniProjectTwo/About'
+// import Header from './component/MiniProjectTwo/Header'
+// import About from './component/MiniProjectTwo/About'
+// import Practice from './component/practice'
+import luna from './component/MiniProjectThree/photos/Luna.jpg'
+import Happy from './component/MiniProjectThree/photos/Happy.webp'
+import Contacts from './component/MiniProjectThree/contacts'
 
 export default function App(){
   return(
-    <>
-    <Header/>
-    <About/>
-    </>
+    <div className="contacts">
+      <Contacts
+      img={luna}
+      name='Luna'
+      phone='0300-PAW-LUNA'
+      email='luna.purrfect@gmail.com'
+      />
+      <Contacts
+      img={Happy}
+      name='Happy'
+      phone='0300-MEOW-HAPPY'
+      email='happy.pawsome@gmail.com'
+      />
+    </div>
+    // <>
+    // {/* <Header/>
+    // <About/> */}
+    // <Practice/>
+    // </>
   )
 }
