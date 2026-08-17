02323443f6b787d5b05288250672291c8d0efdee
Arshiyan Khan <arshiyankhan92@gmail.com>
2026-08-17 22:01:58 +0300

    Added practice.jsx to component folder for practice purpose

---
+++ React/React-concepts/src/component/practice.jsx
@@ -0,0 +1,8 @@
+// Props in JSX
+export default function Practice(){
+    const firstName = 'Arshiyan'
+    const lastName = 'Mairaj'
+    return(
+        <h1>Hello, {firstName} {lastName}</h1>
+    )
+}
