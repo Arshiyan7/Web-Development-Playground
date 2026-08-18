b8d86833589f3fc7866accdd1a61fb4371111876
Arshiyan Khan <arshiyankhan92@gmail.com>
2026-08-18 23:07:03 +0300

    Updated Contacts.jsx

--- React/React-concepts/src/component/MiniProjectThree/contacts.jsx
+++ React/React-concepts/src/component/MiniProjectThree/contacts.jsx
@@ -1,14 +1,32 @@
-export default function Contact(props) {
+// export default function Contact(props) {
+//   return (
+//     <div >
+
+//       <div className="cat-card">
+//         <img src={props.img} alt={props.name} />
+
+//         <h2>{props.name}</h2>
+
+//         <p>{props.phone}</p>
+//         <p>{props.email}</p>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// Destructuring props
+export default function Contact({name, img, phone, email}) {
   return (
     <div >
 
       <div className="cat-card">
-        <img src={props.img} alt="Luna" />
+        <img style={{display: img ? 'block' : 'inline'}} src={img} alt={name} /> // conditional rendering 
 
-        <h2>{props.name}</h2>
+        <h2>{name}</h2>
 
-        <p>{props.phone}</p>
-        <p>{props.email}</p>
+        <p>{phone}</p>
+        <p>{email}</p>
       </div>
 
     </div>
